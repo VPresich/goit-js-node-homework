2173c904620ab4208ec18f020b136a3853c56276
VPresich <valya.alexandrova.presich@gmail.com>
2023-10-08 11:51:39 +0200

    Added scripts

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import './js/module-1/task-1.js';
+import './js/module-1/task-2.js';
+import './js/module-1/task-3.js';
+
+import './js/module-2/task-1.js';
+import './js/module-2/task-2.js';
+import './js/module-2/task-3.js';
+import './js/module-2/task-4.js';
+
+import './js/module-3/task-1.js';
+import './js/module-3/task-2.js';
+import './js/module-3/task-3.js';
