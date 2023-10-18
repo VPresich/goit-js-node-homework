fdf3e8c3e118639835af55649297008fe255f3cf
VPresich <valya.alexandrova.presich@gmail.com>
2023-10-18 21:33:33 +0200

    Added module 6  tasks

--- src/main.js
+++ src/main.js
@@ -19,3 +19,7 @@ import './js/module-5/task-1.js';
 import './js/module-5/task-2.js';
 import './js/module-5/task-3.js';
 import './js/module-5/task-4.js';
+
+import './js/module-6/task-1.js';
+import './js/module-6/task-2.js';
+import './js/module-6/task-3.js';
